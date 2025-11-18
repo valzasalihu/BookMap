17cc2a5640035097e891b0626ea5b26a03f08b4a
haklajalbina <164551749+haklajalbina@users.noreply.github.com>
2025-11-18 07:58:23 +0100

    Delete navbar.js

--- navbar.js
+++
@@ -1,9 +0,0 @@
-// Navbar scroll effect
-window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 50) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
